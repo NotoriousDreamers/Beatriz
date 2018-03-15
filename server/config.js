014e0f08edca3392c52498e544d540034e1ff153
ElizaldeJE <javelisol@gmail.com>
2018-03-15 16:00:51 -0600

    modified /server/config.js

---
+++ server/config.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("./routes");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../client/dist"));
+
+
+
+module.exports = app;
