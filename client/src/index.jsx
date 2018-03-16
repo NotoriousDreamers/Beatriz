36acb154dea8352e84d868718dcc89f1517f4659
ElizaldeJE <javelisol@gmail.com>
2018-03-15 20:51:35 -0600

    got the home page rendering on the localhost

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {BrowserRouter, Route} from 'react-router-dom';
 
+import PrimaryLayout from "./PrimaryLayout.jsx";
 
 class App extends React.Component {
 
   render(){
     return(
       <BrowserRouter>
-
+        <Route path="/" component={PrimaryLayout} />
       </BrowserRouter>
     )}
 };
