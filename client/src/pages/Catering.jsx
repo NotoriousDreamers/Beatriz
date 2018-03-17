7db41379cafb4cecae722ebec069d3de276ce680
ElizaldeJE <javelisol@gmail.com>
2018-03-17 00:32:35 -0600

    this is a simple update on the files, no major changes

--- client/src/pages/Catering.jsx
+++ client/src/pages/Catering.jsx
@@ -2,9 +2,8 @@ import React from "react";
 
 const Cathering = (props) => (
   <div>
-
+    <h1>"This should render in the catering  page" "This is a test"</h1>
   </div>
 )
 
 export default Cathering;
- 
