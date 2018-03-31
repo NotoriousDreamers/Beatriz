0538d1fea1a7167b7af244a5e821478b46394b40
ElizaldeJE <javelisol@gmail.com>
2018-03-31 13:04:43 -0600

    small styling modifications to the header component and the typewritter

--- client/src/components/home/Header.jsx
+++ client/src/components/home/Header.jsx
@@ -1,42 +1,35 @@
 import React from 'react';
 import {Nav,Navbar,NavItem,NavDropdown,MenuItem} from 'react-bootstrap';
+
+
 const Header = (props) => (
+  <div id="Nav">
+    <Navbar>
+<nav className="navbar transparent navbar-fixed-top">
+    <Navbar.Header id="HeaderTacosBeatriz">
+    Tacos Beatriz <small id="LaRoma">La Roma</small>
+  </Navbar.Header>
+  <Nav>
 
-  <section className="foodMenu">
-    <div className="row-1">
-      <div className="col-md-12">
-
-        <div id="Nav">
-          <Navbar>
-            <Navbar.Header>
-              Tacos Beatriz
-            </Navbar.Header>
-            <Nav>
-                  <NavItem eventKey={0} href='/'>
-                   Home
-                  </NavItem>
-                  <NavItem eventKey={1} src="www.tacosbeatriz.com">
-                    Menu
-                  </NavItem>
-                  <NavItem eventKey={2} href="/catering">
-                    Catering
-                  </NavItem>
-                  <NavItem eventKey={2} href="#">
-                    About Us
-                  </NavItem>
-                  <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.4}>Separated link</MenuItem>
-                  </NavDropdown>
-            </Nav>
-          </Navbar>
-        </div>
-      </div>
-    </div>
-  </section>
-
+    <NavItem eventKey={1} href="/menu">
+      Menu
+    </NavItem>
+    <NavItem eventKey={2} href="/catering">
+      Catering
+    </NavItem>
+    <NavItem eventKey={2} href="#">
+      About Us
+    </NavItem>
+    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+      <MenuItem eventKey={3.1}>Action</MenuItem>
+      <MenuItem eventKey={3.2}>Another action</MenuItem>
+      <MenuItem eventKey={3.3}>Something else here</MenuItem>
+      <MenuItem divider />
+      <MenuItem eventKey={3.4}>Separated link</MenuItem>
+    </NavDropdown>
+  </Nav>
+</nav>
+</Navbar>
+  </div>
 )
 export default Header;
--- client/src/components/home/typewriter.jsx
+++ client/src/components/home/typewriter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import TypeWriter from 'react-typewriter';
 import $ from "jquery";
 
-class YourReactClass extends React.Component {
+class TypeWriteR extends React.Component {
 
   render() {
     return (
@@ -12,20 +12,21 @@ class YourReactClass extends React.Component {
         <div className="row-1">
           <div className="col-md-4">
 
-            <img id='panchito' src="https://i.imgur.com/CN4TsM1.jpg" alt="Pancho tipo"/>
-
-
-          </div>
-          <div className="col-md-4">
-
-            <TypeWriter typing={0.9}>The Mexican Revolution (Spanish: Revolución Mexicana) was a major armed struggle c. 1910–1920 radically transforming Mexican culture and government. Although recent research has focused on local and regional aspects of the Revolution, it was a "genuinely national revolution".[4] Its outbreak in 1910 resulted from the failure of the 35-year-long regime of Porfirio Díaz to find a managed solution to the presidential succession. This meant there was a political crisis among competing elites and the opportunity for agrarian insurrection.[5] Wealthy landowner Francisco I. Madero challenged Díaz in the 1910 presidential election, and following the rigged results, revolted under the Plan of San Luis Potosí.[6] Armed conflict ousted Díaz from power and a new election was held in 1911, bringing Madero to the presidency.
-
-</TypeWriter>
+            <img id='panchito' src="https://i.imgur.com/TDsMveO.png" alt="Pancho tipo"/>
 
           </div>
           <div className="col-md-4">
+            <div>
+              <img id='TacosBeatriz' src="https://i.imgur.com/gS6IPvN.png?2" alt="Logo del Restaurante"/>
+            </div>
+            <div>
+              <TypeWriter typing={0.9}>The Mexican Revolution (Spanish: Revolución Mexicana) was a major armed struggle c. 1910–1920 radically transforming Mexican culture and government. Although recent research has focused on local and regional aspects of the Revolution, it was a "genuinely national revolution".[4] Its outbreak in 1910 resulted from the failure of the 35-year-long regime of Porfirio Díaz to find a managed solution to the presidential succession. This meant there was a political crisis among competing elites and the opportunity for agrarian insurrection.[5] Wealthy landowner Francisco I. Madero challenged Díaz in the 1910 presidential election, and following the rigged results, revolted under the Plan of San Luis Potosí.[6] Armed conflict ousted Díaz from power and a new election was held in 1911, bringing Madero to the presidency.
+              </TypeWriter>
+            </div>
+          </div>
+          <div className="col-md-4">
 
-              <h3>"Right column 1st row"</h3>
+            <img id='panchito' src="https://i.imgur.com/TDsMveO.png" alt="Pancho tipo"/>
 
           </div>
         </div>
@@ -37,4 +38,4 @@ class YourReactClass extends React.Component {
 
 }
 
-export default YourReactClass;
+export default TypeWriteR;
