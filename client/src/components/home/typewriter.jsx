c316c5fdd08c722101fde06a3dad3fe6c323f15b
Javier Elizalde <javelisol@gmail.com>
2018-04-13 02:07:08 -0500

    made the component in /client/src/components/home/typewriter.jsx device responsive, no errors, everything is working as it should so far

--- client/src/components/home/typewriter.jsx
+++ client/src/components/home/typewriter.jsx
@@ -1,37 +1,35 @@
 import React from 'react';
-import TypeWriter from 'react-typewriter';
+import { Grid, Row, Col } from "react-bootstrap";
 import $ from "jquery";
 
 class TypeWriteR extends React.Component {
 
   render() {
     return (
-      <section className="foodMenu">
-        <div className="row-1">
-          <div className="col-sm-4">
+      <Grid className="show-grid-BettyYPancho">
+        <Row >
+          <Col xs={6} md={4}>
 
-            <center><img id='beatriz' src="https://i.imgur.com/cWEMVV6.png" alt="Beatriz tipa"/></center>
-
-          </div>
-          <div className="col-sm-4">
-            <div>
-            <img id='Apetisar' src="https://i.imgur.com/gS6IPvN.png?2" alt="   "/>
-              <img id='TacosBeatriz' src="https://i.imgur.com/6V2bNfr.png " alt="   "/>
-
-            </div>
-          </div>
-          <div className="col-sm-4">
-
-            <center><img id='panchito' src="https://i.imgur.com/TDsMveO.png" alt="Pancho tipo"/></center>
-
-          </div>
-        </div>
-      </section>
+          <center><img id='beatriz' src="https://i.imgur.com/cWEMVV6.png" alt="Beatriz tipa"/></center>
 
 
-    );
+          </Col>
+          <Col xs={6} md={4}>
+
+          <img id='TacosBeatriz' src="https://i.imgur.com/gS6IPvN.png?2" alt="TB Logo"/>
+            <img id='Apetisar' src="https://i.imgur.com/6V2bNfr.png " alt="Taquito"/>
+
+          </Col>
+          <Col xs={6} md={4}>
+
+          <center><img id='panchito' src="https://i.imgur.com/TDsMveO.png" alt="Pancho tipo"/></center>
+
+
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
-
-}
+};
 
 export default TypeWriteR;
